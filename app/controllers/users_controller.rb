273255c1dae012368170d9ec3a9eeb5aecc3fdc0
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-01 11:14:16 +0900

    Modify users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :remember_digest)
+      params.require(:user).permit(:name, :email, :password_digest, :password_confirmation)
     end
 end
