c99f466b0a7c42b88db1fffd5ab75e66ecd8e29c
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-15 06:32:09 +0900

    Implement User STI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         log_in @user
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to user_path(@user), notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: user_path(@user) }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -43,8 +43,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to user_path(@user), notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: user_path(@user) }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :type)
     end
 end
