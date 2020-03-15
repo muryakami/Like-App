57049bec41ce1a1c8521bbd844602a453cf7bf4c
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-15 22:27:02 +0900

    Sort application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :jobs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get    '/signup',  to: 'users#new'
   resources :users, except: :new
+  resources :jobs
 end
