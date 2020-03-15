01c16582a9c2c06632ce600fea186885699c09fa
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-15 20:30:15 +0900

    rails generate scaffold Job

---
+++ db/migrate/20200315112849_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :jobs do |t|
+      t.integer :employer_id
+      t.string :name
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
