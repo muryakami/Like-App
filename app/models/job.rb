b45ac0bbe66a98613987efbe8fa5ff36107fe63a
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-15 22:23:12 +0900

    Add validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ApplicationRecord
   belongs_to :employer
+
+  validates :employer_id, presence: true
+  validates :title,       presence: true, length: { maximum: 50 }
+  validates :content,     presence: true, length: { maximum: 2000 }
 end
