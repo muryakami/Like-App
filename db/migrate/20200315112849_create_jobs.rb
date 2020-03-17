ff27dc84c5ba1b4ac2f3391085e77b7cbabe3d7b
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-18 00:22:09 +0900

    Update db_schema

--- db/migrate/20200315112849_create_jobs.rb
+++ db/migrate/20200315112849_create_jobs.rb
@@ -2,7 +2,7 @@ class CreateJobs < ActiveRecord::Migration[6.0]
   def change
     create_table :jobs do |t|
       t.integer :employer_id, null: false
-      t.string :name,         null: false
+      t.string :title,        null: false
       t.text :content,        null: false
 
       t.timestamps
