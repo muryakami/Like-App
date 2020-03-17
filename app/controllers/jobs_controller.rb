92c63de48626b99454743b7f5d4be0a4a70c18c3
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-17 22:51:58 +0900

    Update job_form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -78,6 +78,6 @@ class JobsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def job_params
-      params.require(:job).permit(:employer_id, :name, :content)
+      params.require(:job).permit(:name, :content)
     end
 end
