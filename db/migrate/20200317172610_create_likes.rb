f3d80c98377d001b4d48265bfc647fc2838f320b
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-18 02:34:26 +0900

    rails db:migrate

--- db/migrate/20200317172610_create_likes.rb
+++ db/migrate/20200317172610_create_likes.rb
@@ -1,12 +1,15 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
-      t.integer :employee_id
-      t.integer :job_id
+      t.integer :employee_id, null: false
+      t.integer :job_id,      null: false
       t.boolean :employee_like
       t.boolean :job_like
 
       t.timestamps
     end
+    add_foreign_key :likes, :users, column: :employee_id
+    add_foreign_key :likes, :jobs
+    add_index :likes, [:employee_id, :job_id], unique: true
   end
 end
