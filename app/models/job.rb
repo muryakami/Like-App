ce3740211336b7afdc8549bbd08db48107a56bcc
Yuki Murakami <ymurakami0509@gmail.com>
2020-03-18 00:23:23 +0900

    Update Model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,6 @@ class Job < ApplicationRecord
   belongs_to :employer
 
   validates :employer_id, presence: true
-  validates :name,        presence: true, length: { maximum: 50 }
+  validates :title,       presence: true, length: { maximum: 50 }
   validates :content,     presence: true, length: { maximum: 2000 }
 end
